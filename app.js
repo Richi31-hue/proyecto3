9dec619ef6d9afa0726b00000a0ff6169e6de5a0
Richi31-hue <utm24090336@utma.edu.mx>
2025-07-24 09:25:19 -0600

    Proyecto_3

---
+++ app.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+import cors from "cors"
+import dotenv from "dotenv"
+import express from "express"
+// importar librerias
+
+dotenv.config();//Trayendo configuracion de librerias 
+mongoose.connect(process.env.urlBD)//Conectando a mongoDB
+.then(()=>{
+    console.log("Funciona chingon la base")
+})
+.catch((error)=>{
+    console.log("No funciona la base ptm", error)
+})//Promesa de funcionamiento
+
+const app = express();//Realizado el servidor
+app.use(cors());//Dando seguridad al server 
+app.listen(4000, ()=>{
+    console.log("Se escucha el servidor correctamente")
+})//Escuchando el servidor 
